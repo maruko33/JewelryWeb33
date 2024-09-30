3d3cb23a29f6d8c09b840d38360a48500067857b
Ke Ma <mk617235733@gmail.com>
2024-09-30 18:45:46 -0400

    finished the automated of adds an existing product to the cart

--- tests-jasmine/database/cartTest.js
+++ tests-jasmine/database/cartTest.js
@@ -2,16 +2,36 @@ import * as cart from "../../database/cart.js"
 
 describe('test suite: addToCart', ()=>{
     it('adds an existing product to the cart', () =>{
+        spyOn(localStorage,'setItem');
+        spyOn(localStorage,'getItem').and.callFake(()=>{    //spyOn(object, method wanna use)
+            return JSON.stringify([{
+                productId: '16985746213',
+                quantity:2,
+                deliveryOptionId:'1'
+            }]);                      //in {} we overwrite origin method
+                                                             //we overwrite getItem and return a cart with no item([])
 
+
+        cart.addToCart('16985746213');
+        expect(cart.cart.length).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart.cart[0].productId).toEqual('16985746213');
+        expect(cart.cart[0].quantity).toEqual(2);
+        });
+        cart.loadFromStorage();
     });
 
     it('add a new product to the cart', ()=>{
-        spyOn(localStorage,'getItem').and.callFake(()=>{
-            return JSON.stringify([]);
-            console.log(localStorage.getItem('cart'));
+        spyOn(localStorage, 'setItem');
+        spyOn(localStorage,'getItem').and.callFake(()=>{    //spyOn(object, method wanna use)
+            return JSON.stringify([]);                      //in {} we overwrite origin method
+                                                             //we overwrite getItem and return a cart with no item([])
         });
+        cart.loadFromStorage();
         cart.addToCart('16985746213');
-        let a = cart.cart.length;
         expect(cart.cart.length).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart.cart[0].productId).toEqual('16985746213');
+        expect(cart.cart[0].quantity).toEqual(1);
     });
 })
